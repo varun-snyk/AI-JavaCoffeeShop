bc14718bea20d575553d368e5ac5b06d32d76d10
Varun <varunmano@gmail.com>
2025-05-15 15:48:18 +0100

    added ui and search

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,17 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        // lowercase the input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+
+        // create a query to match the input with the product name or description
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+
+        //execute the query
+        var resultList = (List<Product>) query.getResultList();
+
+        // return the result
+        return resultList;
     }
 
 }
